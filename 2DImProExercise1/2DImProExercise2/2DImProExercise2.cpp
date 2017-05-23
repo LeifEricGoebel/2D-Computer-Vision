61ee2100dd74c71de29d529d5d8e291f51872b8a
Marcel Brusius <marcel.brusius@outlook.de>
2017-05-23 14:49:46 +0200

    - Add Exercise 2 - Add test imges

---
+++ 2DImProExercise1/2DImProExercise2/2DImProExercise2.cpp
@@ -0,0 +1,43 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include "stdafx.h"
+#include <iostream>
+#include <opencv2\imgcodecs.hpp>
+#include <opencv2\features2d.hpp>
+#include <opencv2\photo.hpp>
+#include <opencv2\ximgproc.hpp>
+
+using namespace cv;
+using namespace cv::ximgproc;
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	Mat input;
+	
+	if (argc == 1)
+	{
+		argv[1] = "SaltAndPepper.png";
+	}
+	
+	input = imread(argv[1]);
+	Mat med, bilat, guide, roll;
+	medianBlur(input, med, 5);	
+	imwrite("MedianFilter.png", med);
+
+	bilateralFilter(input, bilat, 5, 1.0, 1.0);
+	imwrite(std::string(argv[1]) + "BilateralFilter.png", bilat);
+
+	guidedFilter(input, input, guide, 5, 0.001, -1);
+	imwrite(std::string(argv[1]) + "GuidedFilter.png", guide);
+
+	rollingGuidanceFilter(input, roll);
+	imwrite(std::string(argv[1]) + "RollingGuidanceFilter.png", roll);
+
+    return 0;
+}
+
