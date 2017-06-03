1f7852c4998cd08a6f5cf6089aa0de554a203b5c
Marcel Brusius <marcel.brusius@outlook.de>
2017-06-03 16:41:36 +0200

    - HOG works so far (but with issues)

--- 2DImProExercise/2DImProExercise3/2DImProExercise3.cpp
+++ 2DImProExercise/2DImProExercise3/2DImProExercise3.cpp
@@ -7,11 +7,30 @@
 
 #include <stdlib.h>
 #include <iostream>
+#include <cmath>
+
+#define PI 3.14159265
 
 using namespace cv;
 using namespace std;
 
-void HistogramOrientedGradient(Mat *image, Mat *output, double cellsize)
+vector<double> ComputeHistogram(Mat *angle, Mat *mag, int d, int index_x, int index_y)
+{
+	vector<double> hist(d);
+	for (int i = 0; i < d; ++i)
+	{
+		for (int j = 0; j < d; ++j)
+		{
+			double x = angle->at<float>(Point(i + index_x*d, j + index_y*d));
+			int mod = (int)floor(x) % 20;
+			mod = mod % 9;
+			hist[mod] = x * mag->at<float>(i, j);
+		}
+	}
+	return hist;
+}
+
+void HistogramOrientedGradient(Mat *image, vector<vector<vector<double> > > *list, Mat *hist, double cellsize)
 {
 	Mat gx, gy;
 	Sobel(*image, gx, CV_32F, 1, 0, 1);
@@ -19,11 +38,57 @@ void HistogramOrientedGradient(Mat *image, Mat *output, double cellsize)
 
 	Mat mag, angle;
 	cartToPolar(gx, gy, mag, angle, 1);
+	Mat *ma = &mag, *an = &angle;
 
-	Size imsize = image->size();
-	Size size(cellsize * floor(imsize.width / cellsize), cellsize * floor(imsize.height / cellsize));
+	size_t size_x = list->size();
+	size_t size_y = (*list)[0].size();
 
-	resize(*image, *output, size);
+	
+
+	for (int i = 0; i < size_x; ++i)
+	{
+		for (int j = 0; j < size_y; ++j)
+		{
+			(*list)[i][j] = ComputeHistogram(an, ma, cellsize, i, j);
+		}
+	}
+
+	for (int i = 0; i < size_x; ++i)
+	{
+		for (int j = 0; j < size_y; ++j)
+		{
+			Mat tmp(cellsize, cellsize, CV_32F); // submatrix for the lines
+			Mat tmp2(cellsize, cellsize, CV_32F); // submatrix of angle
+			for (int k = 0; k < cellsize; ++k)
+			{
+				for (int m = 0; m < cellsize; ++m)
+				{
+					tmp.at<float>(Point(k, m)) = image->at<float>(Point(k + cellsize*i, m + cellsize*j));
+				}
+			}
+
+			// compute line using (sin(x), sin(y)) representation
+			auto Max = std::max_element((*list)[i][j].begin(), (*list)[i][j].end());
+			for (int l = 0; l < 9; ++l)
+			{
+				Point p1(cellsize*sin((180.0 + l*20) * (PI / 180)), cellsize*cos((180.0 + l * 20) * (PI / 180)));
+				Point p2(cellsize*sin((l * 20) * (PI / 180)), cellsize*cos((l * 20) * (PI / 180)));
+
+				double val = (*list)[i][j][l];
+
+				line(tmp, p1, p2,  val / *Max * 255.0);
+			}
+
+			// include calculation into histogram matrix (hist):
+			for (int k = 0; k < cellsize; ++k)
+			{
+				for (int m = 0; m < cellsize; ++m)
+				{
+					hist->at<float>(k + cellsize*i, m + cellsize*j) = tmp.at<float>(k, m);
+				}
+			}
+		}
+	}
 }
 
 int main(int argc, char **argv)
@@ -31,9 +96,6 @@ int main(int argc, char **argv)
 	Mat image = imread(argv[1]);
 	image.convertTo(image, CV_32F, 1 / 255.0);
 	Mat *im = &image;
-
-	Mat resim = image;
-	Mat *res = &resim; // Weil pointer lustig sind
 
 	string str = string(argv[2]);
 	istringstream iss(str);
@@ -50,9 +112,20 @@ int main(int argc, char **argv)
 		return -1;
 	}
 
-	HistogramOrientedGradient(im, res, d); // stod: string to double conversion, stdlib.h
+	Size imsize = image.size();
+	Size size(d * floor(imsize.width / d), d * floor(imsize.height / d));
 
-	imwrite("Test.png", 255 * resim); // war zu einfach, das zurück auf [0,255] zu skalieren :D
+	resize(image, image, size);
+
+	vector<vector<vector<double> > > resim(floor(imsize.width / d), vector<vector<double> >(floor(imsize.height / d), vector<double>(d)));
+	vector<vector<vector<double> > > *res = &resim; // Weil pointer lustig sind
+
+	Mat hist = Mat::zeros(size, CV_32F);
+	Mat *his = &hist;
+
+	HistogramOrientedGradient(im, res, his, d); // stod: string to double conversion, stdlib.h
+
+	imwrite("HOG.png", 255 * hist); // war zu einfach, das zurück auf [0,255] zu skalieren :D
 
     return 0;
 }
